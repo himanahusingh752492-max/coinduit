34ac32a6372e7a85071c13d0f9b35b81b6d10c64
Himanshu Singh <himanshusingh@Himanshus-MacBook-Air.local>
2026-07-09 17:24:32 +0530

    Fix OTP email SMTP

--- server.js
+++ server.js
@@ -14,15 +14,15 @@ const sendOTPEmail = async (email, otp) => {
 
     const transporter = nodemailer.createTransport({
 
-      host: "smtp.gmail.com",
-      port: 465,
-      secure: true,
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
+      secure: false,
 
       family: 4,
 
       auth: {
-        user: process.env.EMAIL,
-        pass: process.env.EMAIL_PASSWORD,
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
       },
 
     });
@@ -30,7 +30,7 @@ const sendOTPEmail = async (email, otp) => {
 
     await transporter.sendMail({
 
-      from: `"CoinDuit" <${process.env.EMAIL}>`,
+      from: `"CoinDuit" <${process.env.SMTP_USER}>`,
       to: email,
       subject: "CoinDuit OTP",
       text: `Your OTP is ${otp}. It is valid for 5 minutes.`,
